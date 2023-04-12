557056b120dcb6a97bca50f0e7fe70c13e2bc99b
Hendrik Reese <38398105+hedare@users.noreply.github.com>
2023-04-12 08:34:20 +0200

    Add files via upload

---
+++ MultiphaseCavBubbleFoam/exampleCases/initbulletJet/initBubbleField.C
@@ -0,0 +1,273 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2008 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    initbubble
+
+Description
+    Initalization utility for example case
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "mathematicalConstants.H"
+#include <iostream>
+#include <fstream>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    volScalarField alpha_water
+    (
+        IOobject
+        (
+            "alpha.water",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+    
+    volScalarField alpha_air
+    (
+        IOobject
+        (
+            "alpha.air",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+    
+    volScalarField alpha_oil
+    (
+        IOobject
+        (
+            "alpha.oil",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+    
+    volScalarField alphas
+    (
+        IOobject
+        (
+            "alphas",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+
+    volScalarField p_rgh
+    (
+        IOobject
+        (
+            "p_rgh",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+
+    volScalarField p
+    (
+        IOobject
+        (
+            "p",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+    
+    volVectorField inkMap
+    (
+        IOobject
+        (
+            "inkMap",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+
+    IOdictionary thermophysicalProperties_water
+    (
+        IOobject
+        (
+            "thermophysicalProperties.water",
+            runTime.constant(),
+            mesh,
+            IOobject::MUST_READ_IF_MODIFIED,
+            IOobject::NO_WRITE
+        )
+    );
+
+    IOdictionary thermophysicalProperties_air
+    (
+        IOobject
+        (
+            "thermophysicalProperties.air",
+            runTime.constant(),
+            mesh,
+            IOobject::MUST_READ_IF_MODIFIED,
+            IOobject::NO_WRITE
+        )
+    );
+
+    dimensionedScalar B_water	("B", 		dimPressure, 	thermophysicalProperties_water.subDict("mixture").subDict("equationOfState"));
+    dimensionedScalar rho0_water	("rho0", 	dimDensity, 	thermophysicalProperties_water.subDict("mixture").subDict("equationOfState"));
+    dimensionedScalar p0_water	("p0", 	dimPressure, 	thermophysicalProperties_water.subDict("mixture").subDict("equationOfState"));
+    dimensionedScalar gamma_water	("gamma", 	dimless, 	thermophysicalProperties_water.subDict("mixture").subDict("equationOfState"));
+
+    dimensionedScalar B_air	("B", 		dimPressure, 	thermophysicalProperties_air.subDict("mixture").subDict("equationOfState"));
+    dimensionedScalar rho0_air	("rho0", 	dimDensity, 	thermophysicalProperties_air.subDict("mixture").subDict("equationOfState"));
+    dimensionedScalar p0_air	("p0", 	dimPressure, 	thermophysicalProperties_air.subDict("mixture").subDict("equationOfState"));
+    dimensionedScalar gamma_air	("gamma", 	dimless, 	thermophysicalProperties_air.subDict("mixture").subDict("equationOfState"));
+
+    Info<< "Reading transportProperties\n" << endl;
+
+    IOdictionary transportProperties
+    (
+        IOobject
+        (
+            "transportProperties",
+            runTime.constant(),
+            mesh,
+            IOobject::MUST_READ_IF_MODIFIED,
+            IOobject::NO_WRITE
+        )
+    );
+
+    Info<< "Reading \n" << endl;
+
+    dimensionedScalar sheetThickness("sheetThickness", dimLength, transportProperties);
+    
+    dimensionedScalar epsDtB_air("epsDtB_air", dimLength*dimLength, transportProperties);
+    
+    dimensionedScalar epsDtB_oil("epsDtB_oil", dimLength*dimLength, transportProperties);
+
+    dimensionedVector BubbleCenter("BubbleCenter", dimLength, transportProperties);
+
+    dimensionedScalar BubbleRadius("BubbleRadius", dimLength, transportProperties);
+
+    dimensionedScalar epsionBubble("epsionBubble", dimless, transportProperties);
+
+    dimensionedScalar pliquid("pliquid", dimPressure, transportProperties);
+
+    dimensionedScalar pBubble("pBubble", dimPressure, transportProperties);
+
+    dimensionedScalar rho = rho0_water*pow((pliquid+B_water)/(p0_water+B_water),1/gamma_water);
+    pBubble = (p0_air + B_air)*pow(rho/rho0_air,gamma_air)-B_air;
+	
+    forAll(alpha_water, cellI)
+    {
+    	vector x = mesh.C()[cellI];
+
+        scalar kb = magSqr((x[0]-BubbleCenter.value()[0])/BubbleRadius.value())+magSqr((x[1]-BubbleCenter.value()[1])/BubbleRadius.value())+magSqr((x[2]-BubbleCenter.value()[2])/BubbleRadius.value());
+
+        alpha_air[cellI] = (1-Foam::tanh((kb-1)/epsionBubble.value()))/2;
+    	
+        if (x[1]>=sheetThickness.value())
+        {
+            alpha_oil[cellI] = 1 - alpha_air[cellI];
+            alpha_air[cellI] = 0;
+        }
+
+        inkMap[cellI] = x - BubbleCenter.value();
+    }
+
+
+//smear the interface to avoid the numerical instability grows in the beginning
+
+    volScalarField alpha_air0(alpha_air);
+    fvScalarMatrix alpha_airSEqn
+    (
+        fvm::Sp(scalar(1),alpha_air) - fvm::laplacian(epsDtB_air,alpha_air) == alpha_air0
+    );
+    alpha_airSEqn.solve();
+
+    if (epsDtB_oil.value() != 0)
+    {
+        volScalarField alpha_oil0(alpha_oil);
+        fvScalarMatrix alpha_oilSEqn
+        (
+            fvm::Sp(scalar(1),alpha_oil) - fvm::laplacian(epsDtB_oil,alpha_oil) == alpha_oil0
+        );
+        alpha_oilSEqn.solve();
+    }
+
+    alpha_water = max(min(1.0-alpha_air-alpha_oil,1.0),0.0);
+    
+    alphas = alpha_oil + 2*alpha_air;
+
+    p = pliquid*(1-alpha_air) + pBubble*alpha_air;
+
+    p_rgh = p;
+         
+    alpha_water.write();
+    
+    alpha_air.write();
+    
+    alpha_oil.write();
+    
+    alphas.write();
+
+    p_rgh.write();
+
+    p.write();
+    
+    inkMap.write();
+    
+    Info<< "End\n" << endl;
+
+    return(0);
+}
+
+
+// ************************************************************************* //
